9fd57432c5ef16cf738c8620c261c467967cd27e
Ashirvad Satapathy <ashirvad.satapathy01@gmail.com>
2023-05-13 05:24:11 +0530

    added api to fetch stock by nifty50 by default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import { getStockByIndices } from "./utils/getStockByIndices.js";
 const app = express();
 app.use(express.json());
+import { NseIndia } from "stock-nse-india"
+const nseIndia = new NseIndia();
 
+app.listen('3000', () => {
+    console.log("Backend is up at port 3000")
+})
+
+app.get('/', async (req, res) => {
+    res.send("Hello World")
+    const upto10 = await getStockByIndices()
+    console.log(upto10)
+})
--- src/utils/getStockByIndices.js
+++ src/utils/getStockByIndices.js
@@ -2,3 +2,19 @@
 //     data.data.map(e => stock.push({ sym: e.symbol, close: e.previousClose }))
 //     console.log(stock)
 // })
+
+import { NseIndia } from "stock-nse-india"
+const nseIndia = new NseIndia();
+
+export async function getStockByIndices(indices = "NIFTY 50") {
+    let stockList = await nseIndia.getEquityStockIndices(indices)
+    const upto10 = []
+    for (let index = 1; index < 11; index++) {
+        upto10.push({
+            symbol: stockList.data[index].symbol,
+            close: stockList.data[index].previousClose
+        })
+    }
+
+    return upto10
+}
